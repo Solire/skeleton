5cca13067bde6dbbd72ec9f0d795c825ff9f226b
thansen <thansen@solire.fr>
2015-01-14 11:38:43 +0100

    Modification de index.php

--- index.php
+++ index.php
@@ -1,6 +1,26 @@
 <?php
+namespace Solire\Lib;
+
+/**
+ * Affichage des erreurs
+ */
+error_reporting(E_ALL & ~E_STRICT);
+ini_set('display_errors', 'Off');
 
 include './vendor/autoload.php';
 
-\Solire\Lib\FrontController::init();
-\Solire\Lib\FrontController::run();
+require './vendor/solire/lib/init.php';
+
+/**
+ * Marvin displays (if true) or sends an email (if false)
+ */
+$debug = false;
+
+//FrontController::setApp([
+//    'name' => 'Xxxxx\Projet',
+//    'dir' => 'Projet',
+//    'namespace' => 'Xxxxx\Projet',
+//    'public' => 'public/projet',
+//]);
+
+include './vendor/solire/lib/start.php';
