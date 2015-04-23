d75dde36249d86159c2b269c4e68cce3b00c49ee
thansen <thansen@solire.fr>
2015-04-23 17:07:50 +0200

    Maj Grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,16 +17,158 @@ module.exports = function (grunt) {
   // Define the configuration for all the tasks
   grunt.initConfig({
     compass : {
-      config: 'config.rb'
+      dist: {
+        options: {
+          outputStyle: 'compressed',
+          sassDir: 'Projet/Front/sass',
+          cssDir: 'public/projet/front/css'
+        }
+      }
     },
-    watch: {
+    composer : {},
+    bower : {
       options: {
-        livereload: true
+        targetDir: 'public/projet/bower_components'
       },
-      css: {
-        files: ['sass/*.scss'],
-        tasks: ['compass']
+      install: {},
+      update: {}
+    },
+    clean: {
+      dist: {
+        files: [
+          {
+            dot: true,
+            src: [
+              'dist/*'
+            ]
+          }
+        ]
+      }
+    },
+    rsync: {
+      options: {
+        args: [
+          '-uazL'
+        ],
+        exclude: [
+          '.git',
+          '.git*',
+
+          '*.default.ini',
+          'local.ini',
+
+          '*.sql',
+          'nb_project',
+          'upload',
+          'db',
+
+          '*.scss',
+          'config.rb',
+          '.sass-cache',
+
+          'nbproject',
+
+          'package.json',
+          'node_modules',
+          'Gruntfile.js',
+
+          '.bowerrc',
+          'bower.json',
+
+          'composer.*',
+          '/dist'
+        ],
+        recursive: true
+      },
+      dist: {
+        options: {
+          src: './',
+          dest: './dist',
+          delete: true
+        }
+      }
+//      production: {
+//        options: {
+//          args: [
+//            '-uavzL'
+//          ],
+//          src: './dist/',
+//          dest: 'www',
+//          host: 'fideliumzu@ftp.fidelium-immobilier.fr'
+//        }
+//      }
+    },
+//    uglify: {
+//      dist: {
+//        files: {
+//        }
+//      }
+//    },
+    copy: {
+      dist: {
+        files: [
+          {
+            src: 'upload/temp',
+            dest: 'dist/upload/temp'
+          }
+        ]
       }
     }
+
+//    replace: {
+//      dist: {
+//        src: 'dist/public/projet/front/js/main.js',
+//        dest: 'dist/public/projet/front/js/main.js',
+//        replacements: [
+//          {
+//            from: /urlArgs\:[^\,]*\,/g,
+//            to: 'urlArgs: "<%= gitinfo.local.branch.current.shortSHA %>",'
+//          }
+//        ]
+//      }
+//    },
+//    gitinfo: {
+//      options: {
+//        cwd: './'
+//      }
+//    }
+
+//    htmlmin: {
+//      dist: {
+//        options: {
+//          removeComments: false,
+//          collapseWhitespace: true
+//        },
+//        files: {
+//          'dist/Projet/Front/view/main.phtml': 'Projet/Front/view/main.phtml'
+//        }
+//      }
+//    }
   });
+
+  // updating server and client libs
+  grunt.registerTask('updateLibs', [
+    'composer:update',
+    'bower:update'
+  ]);
+
+  grunt.registerTask('requireBust', [
+    'gitinfo',
+    'replace'
+  ]);
+
+  // copying and uglifying files to dist directory
+  grunt.registerTask('build', [
+    'clean:dist',
+    'compass:dist',
+    'rsync:dist',
+    'uglify:dist',
+    'copy:dist',
+//    'htmlmin:dist',
+    'requireBust'
+  ]);
+
+//  grunt.registerTask('deploy', [
+//    'rsync:production'
+//  ]);
 };
